4d2345dd6f5c43facaed5b3f439b35a2661bcc28
Syed Bilal Shah <bilxao.bs@gmail.com>
2025-04-27 15:00:24 +0500

    Minor Changes

--- Assets/Standard Assets/Utility/EventSystemChecker.cs
+++ Assets/Standard Assets/Utility/EventSystemChecker.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.EventSystems;
+//using UnityEngine.EventSystems;
 
 public class EventSystemChecker : MonoBehaviour
 {
@@ -10,12 +10,12 @@ public class EventSystemChecker : MonoBehaviour
 	// Use this for initialization
 	void Awake ()
 	{
-	    if(!FindObjectOfType<EventSystem>())
-        {
-           //Instantiate(eventSystem);
-            GameObject obj = new GameObject("EventSystem");
-            obj.AddComponent<EventSystem>();
-            obj.AddComponent<StandaloneInputModule>().forceModuleActive = true;
-        }
+	    //if(!FindObjectOfType<EventSystem>())
+     //   {
+     //      //Instantiate(eventSystem);
+     //       //GameObject obj = new GameObject("EventSystem");
+     //       //obj.AddComponent<EventSystem>();
+     //       obj.AddComponent<StandaloneInputModule>().forceModuleActive = true;
+     //   }
 	}
 }
